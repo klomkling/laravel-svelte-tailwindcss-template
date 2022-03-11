15152f84acb918d473b8c9bac5b5648b756d4f9c
KlomKling <sarawut@idevs.work>
2022-03-11 16:16:31 +0700

    Initial commit

---
+++ resources/js/Pages/index.svelte
@@ -0,0 +1,9 @@
+<script>
+    let name = "world";
+</script>
+
+<main class="w-screen h-screen flex justify-center items-center">
+    <h1 class="text-5xl text-yellow-400">
+        Hello {name} from Svelte through Inertia!
+    </h1>
+</main>
